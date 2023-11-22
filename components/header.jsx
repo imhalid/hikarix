b0ceb0f2708f5f9f0eb59fc253287032091276f4
imhalid <imhalid@icloud.com>
2023-11-22 14:22:38 +0300

    Add animation to header text

--- components/header.jsx
+++ components/header.jsx
@@ -1,34 +1,61 @@
 'use client'
 import { Canvas } from "@react-three/fiber"
 import HeaderText from '@/components/ui/header-text'
-import { OrbitControls } from "@react-three/drei"
+import { motion } from 'framer-motion'
+import { PerspectiveLine } from "@/components/ui/perspective-line"
 export default function Header() {
 
+ const variants = {
+  hidden: {
+   scale: 0,
+   opacity: 0,
+  },
+  visible: {
+   scale: 10,
+   opacity: 1,
+   transition: {
+    duration: 10000,
+    ease: "easeInOut",
+    delay: 0.5,
+   }
+  }
+ }
+
  return (
-  <div className="w-full h-96 border border-neutral-800 relative rounded-2xl my-5 shadow-inner fill-red-400"
+  <div className="w-full h-96 border overflow-hidden border-neutral-800 relative rounded-2xl my-5 shadow-inner">
 
-  >
-
-   <div className="w-full h-full absolute top-0 left-0 invert opacity-30" style={{
-    backgroundImage: "url('https://blog.1a23.com/wp-content/uploads/sites/2/2020/02/pattern-2.svg')",
+   <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center  opacity-30" style={{
+    backgroundImage: "url('/Perspective.svg')",
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
    }}>
-
+    <motion.div
+     initial={{ opacity: 1, y: 0, scale: 3.5 }}
+     animate={{ opacity: 1, y: 300, scale: 0 }}
+     transition={{
+      duration: 5,
+      delay: 0.5,
+      ease: [0, 0.71, 0.2, 1.01]
+     }}
+     className="w-full h-96 rounded-full left-0 bg-black"></motion.div>
    </div>
+
    {/* radial-gradient(circle, rgba(63,94,251,0) 50%, rgba(0,0,0,1) 100%); */}
-   <div className="w-full h-full absolute z-1 top-0 left-0 opacity-100" style={{
+   <div className="w-full h-full absolute z-1 touch-none top-0 left-0 opacity-100" style={{
     background: "radial-gradient(circle, rgba(63,94,251,0) 50%, rgba(0,0,0,1) 100%)",
    }}>
    </div>
 
    <Canvas
     shadows
-    color="#fff"
+    className="touch-none"
     camera={{ position: [0, 0, 1.3] }}
+
    // In order for two dom nodes to be able to receive events they must share
    // the same source. By re-connecting the canvas to a parent that contains the
    // text content as well as the canvas we do just that.
    >
-    {/* <OrbitControls /> */}
     <HeaderText />
    </Canvas>
   </div>
--- components/ui/header-text.jsx
+++ components/ui/header-text.jsx
@@ -1,9 +1,56 @@
 'use client'
+import { useFrame } from '@react-three/fiber'
 import { Text3D, Center, PresentationControls, useMatcapTexture } from "@react-three/drei"
 import Bebas from '@/public/Bebas.json'
+import { useEffect, useRef } from 'react'
+import * as THREE from 'three'
 export default function Header() {
 
  const [matcap] = useMatcapTexture("C7B9A1_F8F1E4_EEE4D2_E4D8C4")
+
+ // when load font and page is finish loading than show text scale animation
+ const ref = useRef()
+ useEffect(() => {
+  ref.current.visible = false
+  const geometry = ref.current.geometry;
+
+  // Geometrinin merkezini hesaplayın
+  geometry.computeBoundingBox();
+  const boundingBox = geometry.boundingBox;
+
+  // BoundingBox'un merkezini bulun
+  const center = new THREE.Vector3();
+  boundingBox.getCenter(center);
+
+  // Geometrinin merkezini ayarlayın
+  geometry.center();
+
+  // Sonra, nesnenin pozisyonunu, önceden hesaplanan merkeze göre ayarlayın
+  ref.current.position.copy(center);
+ }, [])
+
+ // Öncelikle nesnenin geometrisini alın veya oluşturun
+
+
+ useFrame(() => {
+  if (ref.current.visible === false) {
+   ref.current.visible = true;
+   ref.current.scale.set(0, 0, 0);
+   ref.current.originalScale = 1;
+  }
+
+  // scale değerlerini artır
+  if (ref.current.scale.x < 1) {
+   ref.current.scale.x += Math.min(Math.sin(0.01), 1 - ref.current.scale.x);
+  }
+  if (ref.current.scale.y < 1) {
+   ref.current.scale.y += Math.min(Math.sin(0.01), 1 - ref.current.scale.y);
+  }
+  if (ref.current.scale.z < 1) {
+   ref.current.scale.z += Math.min(Math.sin(0.01), 1 - ref.current.scale.z);
+  }
+ })
+
  return (
   <PresentationControls
    snap
@@ -15,6 +62,7 @@ export default function Header() {
   >
    <Center>
     <Text3D
+     ref={ref}
      position={[0, 0, 0]}
      font={Bebas}
      fontSize={10}
