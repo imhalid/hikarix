2702fda2ddab326c33caf2f01db65265c5ab86ac
imhalid <imhalid@icloud.com>
2024-02-18 08:41:27 +0300

    test error

--- app/year/[year]/page.tsx
+++ app/year/[year]/page.tsx
@@ -1,12 +1,12 @@
-import { GET_MOVIE_BY_YEAR } from "@/lib/api";
+import { GET_MOVIES } from "@/lib/api";
 import LoadMoreButton from "@/components/load-more";
 
 export default async function Page({ params }: { params: { year: number } }) {
-  const results = await GET_MOVIE_BY_YEAR(1, params.year);
+  const results = await GET_MOVIES();
   return (
     <div className="flex flex-col md:mr-2">
       <div className="grid-area">{results}</div>
-      <LoadMoreButton YEAR_FUNC={GET_MOVIE_BY_YEAR} year={params.year} />
+      <LoadMoreButton API_FUNC={GET_MOVIES} />
     </div>
   );
 }
